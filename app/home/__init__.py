ba6f03445766f45e0ea88f51ebbbb2dda65d3603
yinuo100823 <534003512@qq.com>
2020-09-03 13:10:37 +0800

    删除home目录

--- app/home/__init__.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @Time : 2020/8/25 9:28
-# @Author : Gery.li
-# @File : __init__.py.py
-from flask import Blueprint
-
-home = Blueprint("home", __name__)
-from . import views
